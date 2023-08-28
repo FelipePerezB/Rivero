01972799b45aa96c7f7b5852f4a68fdb881f62aa
= <=>
2023-08-27 21:55:47 -0400

    webook test 4

--- src/pages/api/webhook.ts
+++ src/pages/api/webhook.ts
@@ -1,21 +1,21 @@
-import type { IncomingHttpHeaders } from 'http';
-import type { NextApiRequest, NextApiResponse } from 'next';
-import type { WebhookRequiredHeaders } from 'svix';
-import type { WebhookEvent } from '@clerk/nextjs/server';
-import { Webhook } from 'svix';
- 
-const webhookSecret = process.env.WEBHOOK_SECRET ;
- 
+import type { IncomingHttpHeaders } from "http";
+import type { NextApiRequest, NextApiResponse } from "next";
+import type { WebhookRequiredHeaders } from "svix";
+import type { WebhookEvent } from "@clerk/nextjs/server";
+import { Webhook } from "svix";
+
+const webhookSecret = process.env.WEBHOOK_SECRET;
+
 export default async function handler(
   req: NextApiRequestWithSvixRequiredHeaders,
   res: NextApiResponse
 ) {
   const payload = JSON.stringify(req.body);
   const headers = req.headers;
-  if(!webhookSecret) return
+  if (!webhookSecret) return;
   // Create a new Webhook instance with your webhook secret
   const wh = new Webhook(webhookSecret);
- 
+
   let evt: WebhookEvent;
   try {
     // Verify the webhook payload and headers
@@ -24,15 +24,23 @@ export default async function handler(
     // If the verification fails, return a 400 error
     return res.status(400).json({});
   }
-  // const { id } = evt.data;
- 
+
   const eventType = evt.type;
-  if (eventType === 'user.created') {
-    console.log(evt);
+  if (eventType === "user.created") {
+    const { id, username, email_addresses, public_metadata } = evt.data;
+
+    console.log({
+      id,
+      username,
+      email_address: email_addresses[0]?.email_address,
+      role: public_metadata?.role,
+      grade: public_metadata?.gradeId,
+      school: public_metadata?.schoolId,
+    });
     res.status(201).json({});
   }
 }
- 
+
 type NextApiRequestWithSvixRequiredHeaders = NextApiRequest & {
   headers: IncomingHttpHeaders & WebhookRequiredHeaders;
 };
