900958a7f4967ec50baff317c51312198e06a265
= <=>
2024-01-26 14:48:48 -0300

    font fix V3

--- src/components/layout/navar/nav-option.tsx
+++ src/components/layout/navar/nav-option.tsx
@@ -35,7 +35,7 @@ export const Option = ({
     // `}
     >
       <Link
-        className={`flex w-full items-center justify-between hover:font-semibold ${
+        className={`flex w-full items-center justify-between hover:font-medium ${
           active
             ? "bg-slate-100 text-blue-600 hover:bg-slate-200"
             : "hover:bg-gray-100/70"
