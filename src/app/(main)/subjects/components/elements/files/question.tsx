306031d476e3e9c1d5ac20b5954c44b6a21e71f3
= <=>
2024-02-26 01:03:22 -0300

    delta: question component children fix

--- src/app/(main)/subjects/components/elements/files/question.tsx
+++ src/app/(main)/subjects/components/elements/files/question.tsx
@@ -66,70 +66,72 @@ export default function Question({
         <div className="flex gap-[0.4em]">
           {number}.- <Paragraph options={{ text: question, indent: false }} />
         </div>
-        {children?.toString() && (
-          <div className="my-2.5">
-            {children?.map((child, i) => (
-              <DynamicElement
-                key={`question-${number}-${child.type}-${i}`}
-                attrs={child}
-                name={child.type}
-              />
-            ))}
-          </div>
-        )}
-        <fieldset className="flex flex-col gap-[0.6em] print:gap-0.5 px-[1em] pt-[0.65em] print:pt-2.5">
-          {alternatives?.split(",")?.map((alternative, i) => {
-            const letter = {
-              0: "A",
-              1: "B",
-              2: "C",
-              3: "D",
-              4: "E",
-            } as any;
-            return (
-              <label
-                className="relative marker:hidden"
-                onClick={() => {
-                  setAnswer && setAnswer({ [number]: letter[i] });
-                  if (
-                    letter[i].toLowerCase().trim() ===
-                    expectedAns.toLowerCase().trim()
-                  ) {
-                    check && setIsCorrect(true);
-                    onSuccess && onSuccess(alternative);
-                  } else {
-                    check && setIsCorrect(false);
-                    onMistake && onMistake(alternative);
-                  }
-                  setCheck && setCheck(false);
-                }}
-                key={alternative + i}
-              >
-                <input
-                  id={"question"}
-                  // onCh
-                  // ref={radioRef}
-                  className="cursor-pointer absolute h-full w-full opacity-0 peer"
-                  type="radio"
-                  value={letter[i]}
-                  name={"alternative" + id}
-                />
-                {/* peer-checked:outline-blue-500 */}
-                <span
-                  className={`text-[0.8em] print:text-[1em] inline-block p-[0.32em] w-full outline outline-gray-200 outline-[0.05em] rounded  peer-checked:outline-[0.14em] print:outline-none bg-white peer-hover:bg-slate-50 
+        <div className="w-full flex gap-[0.8em] items-center">
+          <fieldset className="w-full flex flex-col gap-[0.6em] print:gap-0.5 px-[1em] pt-[0.65em] print:pt-2.5">
+            {alternatives?.split(",")?.map((alternative, i) => {
+              const letter = {
+                0: "A",
+                1: "B",
+                2: "C",
+                3: "D",
+                4: "E",
+              } as any;
+              return (
+                <label
+                  className="relative marker:hidden"
+                  onClick={() => {
+                    setAnswer && setAnswer({ [number]: letter[i] });
+                    if (
+                      letter[i].toLowerCase().trim() ===
+                      expectedAns.toLowerCase().trim()
+                    ) {
+                      check && setIsCorrect(true);
+                      onSuccess && onSuccess(alternative);
+                    } else {
+                      check && setIsCorrect(false);
+                      onMistake && onMistake(alternative);
+                    }
+                    setCheck && setCheck(false);
+                  }}
+                  key={alternative + i}
+                >
+                  <input
+                    id={"question"}
+                    // onCh
+                    // ref={radioRef}
+                    className="cursor-pointer absolute h-full w-full opacity-0 peer"
+                    type="radio"
+                    value={letter[i]}
+                    name={"alternative" + id}
+                  />
+                  {/* peer-checked:outline-blue-500 */}
+                  <span
+                    className={`text-[0.8em] print:text-[1em] inline-block p-[0.32em] w-full outline outline-gray-200 outline-[0.05em] rounded  peer-checked:outline-[0.14em] print:outline-none bg-white peer-hover:bg-slate-50 
                   ${!check && "peer-checked:outline-blue-500"}
                   ${check && isCorrect && "peer-checked:outline-green-500"}
                   ${check && !isCorrect && "peer-checked:outline-red-500"}
                   print:peer-checked:outline-none
                   `}
-                >
-                  <span>{`${letter[i]}) `}</span>
-                  <span>{alternative}</span>
-                </span>
-              </label>
-            );
-          })}
-        </fieldset>
+                  >
+                    <span>{`${letter[i]}) `}</span>
+                    <span>{alternative}</span>
+                  </span>
+                </label>
+              );
+            })}
+          </fieldset>
+          {children?.toString() && (
+            <div className="mx-[2.5em]">
+              {children?.map((child, i) => (
+                <DynamicElement
+                  key={`question-${number}-${child.type}-${i}`}
+                  attrs={child}
+                  name={child.type}
+                />
+              ))}
+            </div>
+          )}
+        </div>
       </article>
     </div>
   );
